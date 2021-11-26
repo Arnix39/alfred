af3d57e354b2b9dad54eb4ca95b1daf6983cd2b6
Arnaud HOTE <arnaud.hote@gmail.com>
2021-11-26 11:27:56 +0000

    Created catkin workspace

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/arnix/src/alfred/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/arnix/udemy_ros/catkin_ws/devel;/home/arnix/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/arnix/udemy_ros/catkin_ws/devel;/home/arnix/catkin_ws/devel;/opt/ros/noetic' != '' else []
