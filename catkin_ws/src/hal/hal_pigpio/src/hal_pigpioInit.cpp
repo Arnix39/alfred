6cc351a3d0464991809577d3d6b4f0268e4521f9
Arnaud HOTE <arnaud.hote@gmail.com>
2021-12-20 15:58:27 +0000

    Updated include files path

--- catkin_ws/src/hal/hal_pigpio/src/hal_pigpioInit.cpp
+++ catkin_ws/src/hal/hal_pigpio/src/hal_pigpioInit.cpp
@@ -1,23 +1,27 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+
+// Pigpio library
 #include <pigpiod_if2.h>
-#include "hal/hal_pigpioGetHandle.h"
-#include "hal/hal_pigpioSetInputMode.h"
-#include "hal/hal_pigpioSetOutputMode.h"
-#include "hal/hal_pigpioSetPwmDutycycle.h"
-#include "hal/hal_pigpioSetPwmFrequency.h"
+
+// Services headers (generated)
+#include "hal_pigpio/hal_pigpioGetHandle.h"
+#include "hal_pigpio/hal_pigpioSetInputMode.h"
+#include "hal_pigpio/hal_pigpioSetOutputMode.h"
+#include "hal_pigpio/hal_pigpioSetPwmDutycycle.h"
+#include "hal_pigpio/hal_pigpioSetPwmFrequency.h"
 
 static int pigpio_handle;
 
-bool getHandle(hal::hal_pigpioGetHandle::Request &req,
-               hal::hal_pigpioGetHandle::Response &res)
+bool getHandle(hal_pigpio::hal_pigpioGetHandle::Request &req,
+               hal_pigpio::hal_pigpioGetHandle::Response &res)
 {
     res.handle = pigpio_handle;
     return true;
 }
 
-bool setInputMode(hal::hal_pigpioSetInputMode::Request &req,
-                  hal::hal_pigpioSetInputMode::Response &res)
+bool setInputMode(hal_pigpio::hal_pigpioSetInputMode::Request &req,
+                  hal_pigpio::hal_pigpioSetInputMode::Response &res)
 {
     if (set_mode(pigpio_handle, req.gpioId, PI_INPUT) == 0)
     {
@@ -31,8 +35,8 @@ bool setInputMode(hal::hal_pigpioSetInputMode::Request &req,
     return true;
 }
 
-bool setOutputMode(hal::hal_pigpioSetOutputMode::Request &req,
-                   hal::hal_pigpioSetOutputMode::Response &res)
+bool setOutputMode(hal_pigpio::hal_pigpioSetOutputMode::Request &req,
+                   hal_pigpio::hal_pigpioSetOutputMode::Response &res)
 {
     if (set_mode(pigpio_handle, req.gpioId, PI_OUTPUT) == 0)
     {
@@ -45,8 +49,8 @@ bool setOutputMode(hal::hal_pigpioSetOutputMode::Request &req,
     return true;
 }
 
-bool setPwmDutycycle(hal::hal_pigpioSetPwmDutycycle::Request &req,
-                     hal::hal_pigpioSetPwmDutycycle::Response &res)
+bool setPwmDutycycle(hal_pigpio::hal_pigpioSetPwmDutycycle::Request &req,
+                     hal_pigpio::hal_pigpioSetPwmDutycycle::Response &res)
 {
     if ((req.dutycycle != 0) && (set_PWM_dutycycle(pigpio_handle, req.gpioId, req.dutycycle) == 0))
     {
@@ -59,8 +63,8 @@ bool setPwmDutycycle(hal::hal_pigpioSetPwmDutycycle::Request &req,
     return true;
 }
 
-bool setPwmFrequency(hal::hal_pigpioSetPwmFrequency::Request &req,
-                     hal::hal_pigpioSetPwmFrequency::Response &res)
+bool setPwmFrequency(hal_pigpio::hal_pigpioSetPwmFrequency::Request &req,
+                     hal_pigpio::hal_pigpioSetPwmFrequency::Response &res)
 {
     int pwmSettingResult = set_PWM_dutycycle(pigpio_handle, req.gpioId, req.frequency);
 
@@ -80,20 +84,16 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "hal_pigpioInit");
     ros::NodeHandle node;
 
-    ros::Rate loop_rate(10);
+    ros::Rate loop_rate(1000);
     pigpio_handle = pigpio_start(NULL, NULL);
 
-    while (ros::ok())
-    {
-        ros::ServiceServer getHandleservice = node.advertiseService("hal_pigpioGetHandle", getHandle);
-        ros::ServiceServer setInputModeservice = node.advertiseService("hal_pigpioSetInputMode", setInputMode);
-        ros::ServiceServer setOutputModeservice = node.advertiseService("hal_pigpioSetOutputMode", setOutputMode);
-        ros::ServiceServer setPwmDutycycleservice = node.advertiseService("hal_pigpioSetPwmDutycycle", setPwmDutycycle);
-        ros::ServiceServer setPwmFrequencyservice = node.advertiseService("hal_pigpioSetPwmFrequency", setPwmFrequency);
+    ros::ServiceServer getHandleService = node.advertiseService("hal_pigpioGetHandle", getHandle);
+    ros::ServiceServer setInputModeService = node.advertiseService("hal_pigpioSetInputMode", setInputMode);
+    ros::ServiceServer setOutputModeService = node.advertiseService("hal_pigpioSetOutputMode", setOutputMode);
+    ros::ServiceServer setPwmDutycycleService = node.advertiseService("hal_pigpioSetPwmDutycycle", setPwmDutycycle);
+    ros::ServiceServer setPwmFrequencyService = node.advertiseService("hal_pigpioSetPwmFrequency", setPwmFrequency);
 
-        ros::spinOnce();
-        loop_rate.sleep();
-    }
+    ros::spin();
 
     pigpio_stop(pigpio_handle);
     return 0;
