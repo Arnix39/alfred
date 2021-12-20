c0625b3efb2c7f0f997f621e1e6763c66b661e56
Arnaud HOTE <arnaud.hote@gmail.com>
2021-12-20 13:31:07 +0000

    Cleaned up hal directory

--- catkin_ws/src/hal/hal_helloWorld/src/hal_helloWorld.cpp
+++ catkin_ws/src/hal/hal_helloWorld/src/hal_helloWorld.cpp
@@ -1,19 +1,14 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
-#include <pigpiod_if2.h>
 
 int main(int argc, char **argv)
 {
-    int pigpio_handle;
     ros::init(argc, argv, "hal_helloWorld");
     ros::NodeHandle node;
 
     ros::Publisher helloWorldPub = node.advertise<std_msgs::String>("HelloWorld", 1000);
 
     ros::Rate loop_rate(1);
-    pigpio_handle = pigpio_start(NULL, NULL);
-    set_PWM_frequency(pigpio_handle, 14, 1000);
-    set_PWM_dutycycle(pigpio_handle, 14, 128);
 
     int count = 0;
     while (ros::ok())
@@ -30,6 +25,5 @@ int main(int argc, char **argv)
         ++count;
     }
 
-    pigpio_stop(pigpio_handle);
     return 0;
 }
