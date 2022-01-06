c0d00379a0d235bd728449e4c396a75ec16364ab
Arnaud HOTE <arnaud.hote@gmail.com>
2022-01-06 12:13:42 +0000

    Define class for inputs

---
+++ catkin_ws/src/hal/hal_pigpio/include/pigpioInput.hpp
@@ -0,0 +1,98 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include <unordered_map>
+
+// Pigpio library
+#include <pigpiod_if2.h>
+
+// Services headers (generated)
+#include "hal_pigpio/hal_pigpioGetHandle.h"
+#include "hal_pigpio/hal_pigpioReadGpio.h"
+#include "hal_pigpio/hal_pigpioSetCallback.h"
+
+struct gpioEdgesNumber
+{
+    uint32_t nbRisingEdges;
+    uint32_t nbFallingEdges;
+};
+
+class PigpioInput
+{
+private:
+    ros::Publisher gpioEdgeChangePub;
+    ros::ServiceServer ReadGpioService;
+    ros::ServiceServer SetCallbackRisingEdgeService;
+    int pigpio_handle;
+    static std::unordered_map<unsigned, gpioEdgesNumber> inputEdges;
+
+public:
+    PigpioInput(ros::NodeHandle *node, int handle)
+    {
+        pigpio_handle = handle;
+
+        ros::Publisher gpioEdgeChangePub = node->advertise<std_msgs::String>("gpioEdgeChange", 1000);
+
+        ros::ServiceServer ReadGpioService = node->advertiseService("hal_pigpioReadGpio", &PigpioInput::readGpio, this);
+        ros::ServiceServer SetCallbackRisingEdgeService = node->advertiseService("hal_pigpioSetCallback", &PigpioInput::setCallback, this);
+    }
+
+    bool readGpio(hal_pigpio::hal_pigpioReadGpio::Request &req,
+                  hal_pigpio::hal_pigpioReadGpio::Response &res)
+    {
+        res.level = gpio_read(pigpio_handle, req.gpioId);
+        if (res.level != PI_BAD_GPIO)
+        {
+            res.result = true;
+        }
+        else
+        {
+            res.result = false;
+        }
+        return true;
+    }
+
+    static void gpioEdgeChangeCallback(int handle, unsigned gpioId, unsigned edgeChangeType, uint32_t timeSinceBoot_us)
+    {
+        auto it = inputEdges.find(gpioId);
+        gpioEdgesNumber nbInputEdges;
+
+        if (it != inputEdges.end())
+        {
+            if (edgeChangeType == 0)
+            {
+                it->second.nbRisingEdges = it->second.nbFallingEdges++;
+            }
+            else
+            {
+                it->second.nbFallingEdges = it->second.nbRisingEdges++;
+            }
+        }
+        else
+        {
+            if (edgeChangeType == 0)
+            {
+                nbInputEdges.nbFallingEdges = 1;
+            }
+            else
+            {
+                nbInputEdges.nbRisingEdges = 1;
+            }
+            inputEdges.insert({gpioId, nbInputEdges});
+        }
+    }
+
+    bool setCallback(hal_pigpio::hal_pigpioSetCallback::Request &req,
+                     hal_pigpio::hal_pigpioSetCallback::Response &res)
+    {
+        res.callbackId = callback(pigpio_handle, req.gpioId, req.edgeChangeType, PigpioInput::gpioEdgeChangeCallback);
+        if (res.callbackId >= 0)
+        {
+            res.result = true;
+        }
+        else
+        {
+            res.result = false;
+        }
+        return true;
+    }
+};
--- catkin_ws/src/hal/hal_pigpio/src/hal_pigpioInput.cpp
+++ catkin_ws/src/hal/hal_pigpio/src/hal_pigpioInput.cpp
@@ -1,62 +1,15 @@
-#include "ros/ros.h"
-#include "std_msgs/String.h"
-
-// Pigpio library
-#include <pigpiod_if2.h>
-
-// Services headers (generated)
-#include "hal_pigpio/hal_pigpioGetHandle.h"
-#include "hal_pigpio/hal_pigpioReadGpio.h"
-#include "hal_pigpio/hal_pigpioSetCallback.h"
-
-static int pigpio_handle;
-
-bool readGpio(hal_pigpio::hal_pigpioReadGpio::Request &req,
-              hal_pigpio::hal_pigpioReadGpio::Response &res)
-{
-    res.level = gpio_read(pigpio_handle, req.gpioId);
-    if (res.level != PI_BAD_GPIO)
-    {
-        res.result = true;
-    }
-    else
-    {
-        res.result = false;
-    }
-    return true;
-}
-
-void gpioEdgeChangeCallback(int handle, unsigned gpioId, unsigned edgeChangeType, uint32_t timeSinceBoot_us)
-{
-}
-
-bool setCallback(hal_pigpio::hal_pigpioSetCallback::Request &req,
-                 hal_pigpio::hal_pigpioSetCallback::Response &res)
-{
-    res.callbackId = callback(pigpio_handle, req.gpioId, req.edgeChangeType, gpioEdgeChangeCallback);
-    if (res.callbackId >= 0)
-    {
-        res.result = true;
-    }
-    else
-    {
-        res.result = false;
-    }
-    return true;
-}
+#include "pigpioInput.hpp"
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "hal_pigpioInput");
     ros::NodeHandle node;
+    int pigpio_handle;
 
     ros::ServiceClient getPigpioHandle = node.serviceClient<hal_pigpio::hal_pigpioGetHandle>("hal_pigpioGetHandle");
     hal_pigpio::hal_pigpioGetHandle pigpioHandleRequest;
     getPigpioHandle.call(pigpioHandleRequest);
     pigpio_handle = pigpioHandleRequest.response.handle;
-
-    ros::ServiceServer ReadGpioService = node.advertiseService("hal_pigpioReadGpio", readGpio);
-    ros::ServiceServer SetCallbackRisingEdgeService = node.advertiseService("hal_pigpioSetCallback", setCallback);
 
     ros::spin();
 
