4c7dd4b19d59387e9b3a038321c440eae605c30d
Arnaud HOTE <arnaud.hote@gmail.com>
2022-01-10 12:26:40 +0000

    Moved methods definiotion in cpp file for output services

--- catkin_ws/src/hal/hal_pigpio/include/pigpioOutput.hpp
+++ catkin_ws/src/hal/hal_pigpio/include/pigpioOutput.hpp
@@ -19,71 +19,13 @@ private:
     int pigpio_handle;
 
 public:
-    PigpioOutput(ros::NodeHandle *node, int handle)
-    {
-        pigpio_handle = handle;
-
-        ros::ServiceServer setPwmDutycycleService = node->advertiseService("hal_pigpioSetPwmDutycycle", &PigpioOutput::setPwmDutycycle, this);
-        ros::ServiceServer setPwmFrequencyService = node->advertiseService("hal_pigpioSetPwmFrequency", &PigpioOutput::setPwmFrequency, this);
-        ros::ServiceServer setGpioHighService = node->advertiseService("hal_pigpioSetGpioHigh", &PigpioOutput::setGpioHigh, this);
-        ros::ServiceServer setGpioLowService = node->advertiseService("hal_pigpioSetGpioLow", &PigpioOutput::setGpioLow, this);
-    }
-
+    PigpioOutput(ros::NodeHandle *node, int handle);
     bool setPwmDutycycle(hal_pigpio::hal_pigpioSetPwmDutycycle::Request &req,
-                         hal_pigpio::hal_pigpioSetPwmDutycycle::Response &res)
-    {
-        if ((req.dutycycle != 0) && (set_PWM_dutycycle(pigpio_handle, req.gpioId, req.dutycycle) == 0))
-        {
-            res.result = true;
-        }
-        else
-        {
-            res.result = false;
-        }
-        return true;
-    }
-
+                         hal_pigpio::hal_pigpioSetPwmDutycycle::Response &res);
     bool setPwmFrequency(hal_pigpio::hal_pigpioSetPwmFrequency::Request &req,
-                         hal_pigpio::hal_pigpioSetPwmFrequency::Response &res)
-    {
-        int pwmSettingResult = set_PWM_dutycycle(pigpio_handle, req.gpioId, req.frequency);
-
-        if ((pwmSettingResult != PI_NOT_PERMITTED) && (pwmSettingResult != PI_BAD_USER_GPIO))
-        {
-            res.result = true;
-        }
-        else
-        {
-            res.result = false;
-        }
-        return true;
-    }
-
+                         hal_pigpio::hal_pigpioSetPwmFrequency::Response &res);
     bool setGpioHigh(hal_pigpio::hal_pigpioSetGpioHigh::Request &req,
-                     hal_pigpio::hal_pigpioSetGpioHigh::Response &res)
-    {
-        if ((gpio_write(pigpio_handle, req.gpioId, PI_HIGH) != 0))
-        {
-            res.result = true;
-        }
-        else
-        {
-            res.result = false;
-        }
-        return true;
-    }
-
+                     hal_pigpio::hal_pigpioSetGpioHigh::Response &res);
     bool setGpioLow(hal_pigpio::hal_pigpioSetGpioLow::Request &req,
-                    hal_pigpio::hal_pigpioSetGpioLow::Response &res)
-    {
-        if ((gpio_write(pigpio_handle, req.gpioId, PI_LOW) != 0))
-        {
-            res.result = true;
-        }
-        else
-        {
-            res.result = false;
-        }
-        return true;
-    }
+                    hal_pigpio::hal_pigpioSetGpioLow::Response &res);
 };
--- catkin_ws/src/hal/hal_pigpio/src/hal_pigpioOutput.cpp
+++ catkin_ws/src/hal/hal_pigpio/src/hal_pigpioOutput.cpp
@@ -3,6 +3,74 @@
 // Services headers (generated)
 #include "hal_pigpio/hal_pigpioGetHandle.h"
 
+PigpioOutput::PigpioOutput(ros::NodeHandle *node, int handle)
+{
+    pigpio_handle = handle;
+
+    ros::ServiceServer setPwmDutycycleService = node->advertiseService("hal_pigpioSetPwmDutycycle", &PigpioOutput::setPwmDutycycle, this);
+    ros::ServiceServer setPwmFrequencyService = node->advertiseService("hal_pigpioSetPwmFrequency", &PigpioOutput::setPwmFrequency, this);
+    ros::ServiceServer setGpioHighService = node->advertiseService("hal_pigpioSetGpioHigh", &PigpioOutput::setGpioHigh, this);
+    ros::ServiceServer setGpioLowService = node->advertiseService("hal_pigpioSetGpioLow", &PigpioOutput::setGpioLow, this);
+}
+
+bool PigpioOutput::setPwmDutycycle(hal_pigpio::hal_pigpioSetPwmDutycycle::Request &req,
+                                   hal_pigpio::hal_pigpioSetPwmDutycycle::Response &res)
+{
+    if ((req.dutycycle != 0) && (set_PWM_dutycycle(pigpio_handle, req.gpioId, req.dutycycle) == 0))
+    {
+        res.result = true;
+    }
+    else
+    {
+        res.result = false;
+    }
+    return true;
+}
+
+bool PigpioOutput::setPwmFrequency(hal_pigpio::hal_pigpioSetPwmFrequency::Request &req,
+                                   hal_pigpio::hal_pigpioSetPwmFrequency::Response &res)
+{
+    int pwmSettingResult = set_PWM_dutycycle(pigpio_handle, req.gpioId, req.frequency);
+
+    if ((pwmSettingResult != PI_NOT_PERMITTED) && (pwmSettingResult != PI_BAD_USER_GPIO))
+    {
+        res.result = true;
+    }
+    else
+    {
+        res.result = false;
+    }
+    return true;
+}
+
+bool PigpioOutput::setGpioHigh(hal_pigpio::hal_pigpioSetGpioHigh::Request &req,
+                               hal_pigpio::hal_pigpioSetGpioHigh::Response &res)
+{
+    if ((gpio_write(pigpio_handle, req.gpioId, PI_HIGH) != 0))
+    {
+        res.result = true;
+    }
+    else
+    {
+        res.result = false;
+    }
+    return true;
+}
+
+bool PigpioOutput::setGpioLow(hal_pigpio::hal_pigpioSetGpioLow::Request &req,
+                              hal_pigpio::hal_pigpioSetGpioLow::Response &res)
+{
+    if ((gpio_write(pigpio_handle, req.gpioId, PI_LOW) != 0))
+    {
+        res.result = true;
+    }
+    else
+    {
+        res.result = false;
+    }
+    return true;
+}
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "hal_pigpioOutput");
