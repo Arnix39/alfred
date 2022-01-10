dadc6bd5880aa5d0b710753b3bca39c84dd16e89
Arnaud HOTE <arnaud.hote@gmail.com>
2022-01-10 12:18:17 +0000

    Moved methods definiotion in cpp file for I2C services

--- catkin_ws/src/hal/hal_pigpio/include/pigpioI2c.hpp
+++ catkin_ws/src/hal/hal_pigpio/include/pigpioI2c.hpp
@@ -15,40 +15,9 @@ private:
     int pigpio_handle;
 
 public:
-    PigpioI2c(ros::NodeHandle *node, int handle)
-    {
-        pigpio_handle = handle;
-
-        ros::ServiceServer i2cOpenService = node->advertiseService("hal_pigpioI2cOpen", &PigpioI2c::i2cOpen, this);
-        ros::ServiceServer i2cCloseService = node->advertiseService("hal_pigpioI2cClose", &PigpioI2c::i2cClose, this);
-    }
-
+    PigpioI2c(ros::NodeHandle *node, int handle);
     bool i2cOpen(hal_pigpio::hal_pigpioI2cOpen::Request &req,
-                 hal_pigpio::hal_pigpioI2cOpen::Response &res)
-    {
-        res.handle = i2c_open(pigpio_handle, req.bus, req.address, 0);
-        if (res.handle >= 0)
-        {
-            res.result = true;
-        }
-        else
-        {
-            res.result = false;
-        }
-        return true;
-    }
-
+                 hal_pigpio::hal_pigpioI2cOpen::Response &res);
     bool i2cClose(hal_pigpio::hal_pigpioI2cClose::Request &req,
-                  hal_pigpio::hal_pigpioI2cClose::Response &res)
-    {
-        if (i2c_close(pigpio_handle, req.handle) == 0)
-        {
-            res.result = true;
-        }
-        else
-        {
-            res.result = false;
-        }
-        return true;
-    }
+                  hal_pigpio::hal_pigpioI2cClose::Response &res);
 };
--- catkin_ws/src/hal/hal_pigpio/src/hal_pigpioI2c.cpp
+++ catkin_ws/src/hal/hal_pigpio/src/hal_pigpioI2c.cpp
@@ -3,6 +3,43 @@
 // Services headers (generated)
 #include "hal_pigpio/hal_pigpioGetHandle.h"
 
+PigpioI2c::PigpioI2c(ros::NodeHandle *node, int handle)
+{
+    pigpio_handle = handle;
+
+    ros::ServiceServer i2cOpenService = node->advertiseService("hal_pigpioI2cOpen", &PigpioI2c::i2cOpen, this);
+    ros::ServiceServer i2cCloseService = node->advertiseService("hal_pigpioI2cClose", &PigpioI2c::i2cClose, this);
+}
+
+bool PigpioI2c::i2cOpen(hal_pigpio::hal_pigpioI2cOpen::Request &req,
+                        hal_pigpio::hal_pigpioI2cOpen::Response &res)
+{
+    res.handle = i2c_open(pigpio_handle, req.bus, req.address, 0);
+    if (res.handle >= 0)
+    {
+        res.result = true;
+    }
+    else
+    {
+        res.result = false;
+    }
+    return true;
+}
+
+bool PigpioI2c::i2cClose(hal_pigpio::hal_pigpioI2cClose::Request &req,
+                         hal_pigpio::hal_pigpioI2cClose::Response &res)
+{
+    if (i2c_close(pigpio_handle, req.handle) == 0)
+    {
+        res.result = true;
+    }
+    else
+    {
+        res.result = false;
+    }
+    return true;
+}
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "hal_pigpioI2c");
