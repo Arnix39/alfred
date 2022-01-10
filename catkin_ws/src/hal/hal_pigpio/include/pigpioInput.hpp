b0844111c8b801be5648444fd8597f7711d1b20e
Arnaud HOTE <arnaud.hote@gmail.com>
2022-01-10 12:25:10 +0000

    Moved methods definiotion in cpp file for input services

--- catkin_ws/src/hal/hal_pigpio/include/pigpioInput.hpp
+++ catkin_ws/src/hal/hal_pigpio/include/pigpioInput.hpp
@@ -19,54 +19,10 @@ private:
     int pigpio_handle;
 
 public:
-    PigpioInput(ros::NodeHandle *node, int handle)
-    {
-        pigpio_handle = handle;
-
-        ros::Publisher gpioEdgeChangePub = node->advertise<hal_pigpio::hal_pgpioEdgeChangeMsg>("gpioEdgeChange", 1000);
-
-        ros::ServiceServer readGpioService = node->advertiseService("hal_pigpioReadGpio", &PigpioInput::readGpio, this);
-        ros::ServiceServer setCallbackRisingEdgeService = node->advertiseService("hal_pigpioSetCallback", &PigpioInput::setCallback, this);
-    }
-
+    PigpioInput(ros::NodeHandle *node, int handle);
     bool readGpio(hal_pigpio::hal_pigpioReadGpio::Request &req,
-                  hal_pigpio::hal_pigpioReadGpio::Response &res)
-    {
-        res.level = gpio_read(pigpio_handle, req.gpioId);
-        if (res.level != PI_BAD_GPIO)
-        {
-            res.result = true;
-        }
-        else
-        {
-            res.result = false;
-        }
-        return true;
-    }
-
-    static void gpioEdgeChangeCallback(int handle, unsigned gpioId, unsigned edgeChangeType, uint32_t timeSinceBoot_us)
-    {
-        hal_pigpio::hal_pgpioEdgeChangeMsg edgeChangeMsg;
-
-        edgeChangeMsg.gpioId = gpioId;
-        edgeChangeMsg.edgeChangeType = edgeChangeType;
-        edgeChangeMsg.timeSinceBoot_us = timeSinceBoot_us;
-
-        gpioEdgeChangePub.publish(edgeChangeMsg);
-    }
-
+                  hal_pigpio::hal_pigpioReadGpio::Response &res);
+    static void gpioEdgeChangeCallback(int handle, unsigned gpioId, unsigned edgeChangeType, uint32_t timeSinceBoot_us);
     bool setCallback(hal_pigpio::hal_pigpioSetCallback::Request &req,
-                     hal_pigpio::hal_pigpioSetCallback::Response &res)
-    {
-        res.callbackId = callback(pigpio_handle, req.gpioId, req.edgeChangeType, PigpioInput::gpioEdgeChangeCallback);
-        if (res.callbackId >= 0)
-        {
-            res.result = true;
-        }
-        else
-        {
-            res.result = false;
-        }
-        return true;
-    }
+                     hal_pigpio::hal_pigpioSetCallback::Response &res);
 };
--- catkin_ws/src/hal/hal_pigpio/src/hal_pigpioInput.cpp
+++ catkin_ws/src/hal/hal_pigpio/src/hal_pigpioInput.cpp
@@ -3,6 +3,57 @@
 // Services headers (generated)
 #include "hal_pigpio/hal_pigpioGetHandle.h"
 
+PigpioInput::PigpioInput(ros::NodeHandle *node, int handle)
+{
+    pigpio_handle = handle;
+
+    ros::Publisher gpioEdgeChangePub = node->advertise<hal_pigpio::hal_pgpioEdgeChangeMsg>("gpioEdgeChange", 1000);
+
+    ros::ServiceServer readGpioService = node->advertiseService("hal_pigpioReadGpio", &PigpioInput::readGpio, this);
+    ros::ServiceServer setCallbackRisingEdgeService = node->advertiseService("hal_pigpioSetCallback", &PigpioInput::setCallback, this);
+}
+
+bool PigpioInput::readGpio(hal_pigpio::hal_pigpioReadGpio::Request &req,
+                           hal_pigpio::hal_pigpioReadGpio::Response &res)
+{
+    res.level = gpio_read(pigpio_handle, req.gpioId);
+    if (res.level != PI_BAD_GPIO)
+    {
+        res.result = true;
+    }
+    else
+    {
+        res.result = false;
+    }
+    return true;
+}
+
+void PigpioInput::gpioEdgeChangeCallback(int handle, unsigned gpioId, unsigned edgeChangeType, uint32_t timeSinceBoot_us)
+{
+    hal_pigpio::hal_pgpioEdgeChangeMsg edgeChangeMsg;
+
+    edgeChangeMsg.gpioId = gpioId;
+    edgeChangeMsg.edgeChangeType = edgeChangeType;
+    edgeChangeMsg.timeSinceBoot_us = timeSinceBoot_us;
+
+    gpioEdgeChangePub.publish(edgeChangeMsg);
+}
+
+bool PigpioInput::setCallback(hal_pigpio::hal_pigpioSetCallback::Request &req,
+                              hal_pigpio::hal_pigpioSetCallback::Response &res)
+{
+    res.callbackId = callback(pigpio_handle, req.gpioId, req.edgeChangeType, PigpioInput::gpioEdgeChangeCallback);
+    if (res.callbackId >= 0)
+    {
+        res.result = true;
+    }
+    else
+    {
+        res.result = false;
+    }
+    return true;
+}
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "hal_pigpioInput");
