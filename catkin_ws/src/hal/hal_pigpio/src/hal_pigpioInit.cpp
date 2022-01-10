d5e55e4a0bdb644d2b685849336b55708d14e75c
Arnaud HOTE <arnaud.hote@gmail.com>
2022-01-10 12:01:11 +0000

    Created class for init services

--- catkin_ws/src/hal/hal_pigpio/src/hal_pigpioInit.cpp
+++ catkin_ws/src/hal/hal_pigpio/src/hal_pigpioInit.cpp
@@ -1,71 +1,11 @@
-#include "ros/ros.h"
-#include "std_msgs/String.h"
-
-// Pigpio library
-#include <pigpiod_if2.h>
-
-// Services headers (generated)
-#include "hal_pigpio/hal_pigpioGetHandle.h"
-#include "hal_pigpio/hal_pigpioSetInputMode.h"
-#include "hal_pigpio/hal_pigpioSetOutputMode.h"
-
-static int pigpio_handle;
-
-bool getHandle(hal_pigpio::hal_pigpioGetHandle::Request &req,
-               hal_pigpio::hal_pigpioGetHandle::Response &res)
-{
-    res.handle = pigpio_handle;
-    return true;
-}
-
-bool setInputMode(hal_pigpio::hal_pigpioSetInputMode::Request &req,
-                  hal_pigpio::hal_pigpioSetInputMode::Response &res)
-{
-    if (set_mode(pigpio_handle, req.gpioId, PI_INPUT) == 0)
-    {
-        res.result = true;
-    }
-    else
-    {
-        res.result = false;
-    }
-
-    return true;
-}
-
-bool setOutputMode(hal_pigpio::hal_pigpioSetOutputMode::Request &req,
-                   hal_pigpio::hal_pigpioSetOutputMode::Response &res)
-{
-    if (set_mode(pigpio_handle, req.gpioId, PI_OUTPUT) == 0)
-    {
-        res.result = true;
-    }
-    else
-    {
-        res.result = false;
-    }
-    return true;
-}
+#include "pigpioInit.hpp"
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "hal_pigpioInit");
     ros::NodeHandle node;
 
-    pigpio_handle = pigpio_start(NULL, NULL);
-
-    if (pigpio_handle < 0)
-    {
-        ROS_ERROR("Failed to start pigpio daemon");
-        return 1;
-    }
-
-    ros::ServiceServer getHandleService = node.advertiseService("hal_pigpioGetHandle", getHandle);
-    ros::ServiceServer setInputModeService = node.advertiseService("hal_pigpioSetInputMode", setInputMode);
-    ros::ServiceServer setOutputModeService = node.advertiseService("hal_pigpioSetOutputMode", setOutputMode);
-
     ros::spin();
 
-    pigpio_stop(pigpio_handle);
     return 0;
 }
