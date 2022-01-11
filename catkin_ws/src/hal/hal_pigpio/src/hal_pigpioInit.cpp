0c844fead5053be5dff9d5595b9610c1f6100bbd
Arnaud HOTE <arnaud.hote@gmail.com>
2022-01-11 11:28:27 +0000

    Added creation of init object

--- catkin_ws/src/hal/hal_pigpio/src/hal_pigpioInit.cpp
+++ catkin_ws/src/hal/hal_pigpio/src/hal_pigpioInit.cpp
@@ -60,6 +60,8 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "hal_pigpioInit");
     ros::NodeHandle node;
 
+    PigpioInit pigpioInit = PigpioInit(&node);
+
     ros::spin();
 
     return 0;
