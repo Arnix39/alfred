221289bf1704de93a2f9b12d3e5b55fec5f0cf63
Arnaud HOTE <arnaud.hote@gmail.com>
2022-01-21 18:52:51 +0000

    Added configureGpios method for proxsens object

--- catkin_ws/src/hal/hal_proxsens/include/hal_proxSens.hpp
+++ catkin_ws/src/hal/hal_proxsens/include/hal_proxSens.hpp
@@ -1,16 +1,38 @@
 #include "ros/ros.h"
 
 // Services and messages headers (generated)
+#include "hal_pigpio/hal_pigpioSetInputMode.h"
+#include "hal_pigpio/hal_pigpioSetOutputMode.h"
 #include "hal_pigpio/hal_pigpioSetGpioHigh.h"
 #include "hal_pigpio/hal_pigpioSetGpioLow.h"
 #include "hal_pigpio/hal_pigpioSetCallback.h"
 #include "hal_pigpio/hal_pigpioEdgeChangeMsg.h"
 #include "hal_proxsens/hal_proxsensMsg.h"
 
+#define PROXSENS_TRIGGER_GPIO 5
+#define PROXSENS_ECHO_GPIO 6
+
+#define RISING_EDGE     0
+#define FALLING_EDGE    1
+#define EITHER_EDGE     2
+
 class ProxSens
 {
 private:
+    ros::Subscriber edgeChangeSub;
+    ros::Publisher proxSensPub;
+    ros::ServiceClient gpioSetInputClient;
+    ros::ServiceClient gpioSetCallbackClient;
+    ros::ServiceClient gpioSetOutputClient;
+    uint8_t gpio;
+    uint8_t edgeChangeType;
+    uint32_t timestamp;
+    uint32_t echoCallbackId;
+    void edgeChangeCallback(const hal_pigpio::hal_pigpioEdgeChangeMsg& msg);
 
 public:
-
+    ProxSens(ros::NodeHandle *node);
+    void publishMessage(void);
+    void configureGpios(void);
+    void trigger(void);
 };
--- catkin_ws/src/hal/hal_proxsens/src/hal_proxSens.cpp
+++ catkin_ws/src/hal/hal_proxsens/src/hal_proxSens.cpp
@@ -1,20 +1,72 @@
 #include "hal_proxSens.hpp"
 
+ProxSens::ProxSens(ros::NodeHandle *node)
+{
+    proxSensPub = node->advertise<hal_proxsens::hal_proxsensMsg>("proxSensorValue", 1000);
+    edgeChangeSub = node->subscribe("gpioEdgeChange", 1000, &ProxSens::edgeChangeCallback, this);
+
+    gpioSetInputClient = node->serviceClient<hal_pigpio::hal_pigpioSetInputMode>("hal_pigpioSetInputMode");
+    gpioSetOutputClient = node->serviceClient<hal_pigpio::hal_pigpioSetOutputMode>("hal_pigpioSetOutputMode");
+}
+
+void ProxSens::edgeChangeCallback(const hal_pigpio::hal_pigpioEdgeChangeMsg& msg)
+{
+    gpio = msg.gpioId;
+    edgeChangeType = msg.edgeChangeType;
+    timestamp = msg.timeSinceBoot_us;
+}
+
+void ProxSens::publishMessage(hal_proxsens::hal_proxsensMsg message)
+{
+    hal_proxsens::hal_proxsensMsg proxSensMessage = 0; 
+    proxSensMessage.distanceInCm = 100;
+
+    proxSensPub.publish(message);
+}
+
+void ProxSens::configureGpios(void)
+{
+    hal_pigpio::hal_pigpioSetInputMode setInputModeSrv;
+    hal_pigpio::hal_pigpioSetCallback setCallbackSrv;
+    hal_pigpio::hal_pigpioSetOutputMode setOutputModeSrv;
+
+    setInputModeSrv.request.gpioId = PROXSENS_ECHO_GPIO;
+    
+    setCallbackSrv.request.gpioId = PROXSENS_ECHO_GPIO;
+    setCallbackSrv.request.edgeChangeType = EITHER_EDGE;
+    
+    setOutputModeSrv.request.gpioId = PROXSENS_TRIGGER_GPIO;
+
+    gpioSetInputClient.call(setInputModeSrv);
+    
+    gpioSetCallbackClient.call(setCallbackSrv);
+    if(setCallbackSrv.response.hasSucceeded)
+    {
+        echoCallbackId = setCallbackSrv.response.callbackId;
+    }
+
+    gpioSetOutputClient.call(setOutputModeSrv);
+}
+
+void ProxSens::trigger(void)
+{
+
+}
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "hal_proxsens");
     ros::NodeHandle node;
 
-    ros::Publisher proxSensPub = node.advertise<hal_proxsens::hal_proxsensMsg>("proxSensorValue", 1000);
+    ProxSens proxSens = ProxSens(&node);
+    proxSens.configureGpios();
 
     ros::Rate loop_rate(10);
 
     while (ros::ok())
     {
-        hal_proxsens::hal_proxsensMsg proxSensMessage; 
-        proxSensMessage.distanceInCm = 100;
-
-        proxSensPub.publish(proxSensMessage);
+        proxSens.publishMessage();
+        proxSens.trigger();
 
         ros::spinOnce();
         loop_rate.sleep();
