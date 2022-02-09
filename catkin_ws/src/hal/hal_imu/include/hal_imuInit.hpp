2ef13be2a3d253b7dc23fc2112b7536194ef1090
Arnaud HOTE <arnaud.hote@gmail.com>
2022-02-09 13:26:20 +0000

    Created init node for hal_imu

---
+++ catkin_ws/src/hal/hal_imu/include/hal_imuInit.hpp
@@ -0,0 +1,7 @@
+#ifndef HAL_IMU
+#define HAL_IMU
+
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+#endif
---
+++ catkin_ws/src/hal/hal_imu/src/hal_imuInit.cpp
@@ -0,0 +1,20 @@
+#include "hal_imuInit.hpp"
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "hal_imu");
+    ros::NodeHandle node;
+
+    ros::Publisher imuPub = node.advertise<std_msgs::String>("IMUValues", 1000);
+
+    std_msgs::String imuMessage;
+    std::stringstream stream;
+    stream << "IMU values: NaN";
+    imuMessage.data = stream.str();
+
+    imuPub.publish(imuMessage);
+
+    ros::spin();
+
+    return 0;
+}
