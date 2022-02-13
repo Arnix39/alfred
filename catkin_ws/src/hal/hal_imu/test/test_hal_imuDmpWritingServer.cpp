0ca457ea37da33faa02ed7627399e7f178901dec
Arnaud HOTE <arnaud.hote@gmail.com>
2022-02-13 15:16:23 +0000

    Added test for hal_imuDmpWritingServer

---
+++ catkin_ws/src/hal/hal_imu/test/test_hal_imuDmpWritingServer.cpp
@@ -0,0 +1,74 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+#include "hal_imuDmpWritingServer.hpp"
+
+/* Action server interface mock */
+class ImuActionServerMock : public ImuActionServer
+{
+private:
+    ros::NodeHandle *nodeHandle;
+    imuActionServer_t imuWriteDmpServermock;
+
+public:
+    ImuActionServerMock() = default;
+    ~ImuActionServerMock() = default;
+    void registerCallback(ImuDmpWritingServer *imuDmpWritingServer) override;
+    imuActionServer_t *getActionServerHandle() override;
+};
+
+void ImuActionServerMock::registerCallback(ImuDmpWritingServer *imuDmpWritingServer)
+{
+}
+
+imuActionServer_t *ImuActionServerMock::getActionServerHandle()
+{
+    return &imuWriteDmpServermock;
+}
+
+/* Services interface mock */
+class ImuClientsMock : public ImuClients
+{
+private:
+    ros::ServiceClient i2cReadByteDataClientRos;
+    ros::ServiceClient i2cWriteByteDataClientRos;
+
+public:
+    ImuClientsMock() = default;
+    ~ImuClientsMock() = default;
+    ros::ServiceClient *getReadByteDataClientHandle() override;
+    ros::ServiceClient *getWriteByteDataClientHandle() override;
+};
+
+ros::ServiceClient *ImuClientsMock::getReadByteDataClientHandle()
+{
+    return &i2cReadByteDataClientRos;
+}
+
+ros::ServiceClient *ImuClientsMock::getWriteByteDataClientHandle()
+{
+    return &i2cWriteByteDataClientRos;
+}
+
+/* Test fixtures */
+class ImuDmpWritingServerTest : public testing::Test
+{
+protected:
+    ros::NodeHandle node;
+    ImuActionServerMock imuWriteDmpServer;
+    ImuClientsMock imuServiceClients;
+    ImuDmpWritingServer imuDmpWritingServer = ImuDmpWritingServer(&imuWriteDmpServer, &imuServiceClients);
+};
+
+/* Test cases */
+TEST_F(ImuDmpWritingServerTest, sensorDistanceDefaultValue)
+{
+    ASSERT_TRUE(true);
+}
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    ros::init(argc, argv, "test_hal_imu");
+    return RUN_ALL_TESTS();
+}
