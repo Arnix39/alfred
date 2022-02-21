bd2fb1dda43ccc66f3785dd4d7f11db57954e5d0
Arnaud HOTE <arnaud.hote@gmail.com>
2022-02-21 16:56:56 +0000

    Corrected name of variable

--- catkin_ws/src/hal/hal_pigpio/src/hal_pigpioOutput.cpp
+++ catkin_ws/src/hal/hal_pigpio/src/hal_pigpioOutput.cpp
@@ -95,14 +95,14 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "hal_pigpioOutput");
     ros::NodeHandle node;
-    int pigpio_handle;
+    int pigpioHandle;
 
     ros::ServiceClient getPigpioHandle = node.serviceClient<hal_pigpio::hal_pigpioGetHandle>("hal_pigpioGetHandle");
     hal_pigpio::hal_pigpioGetHandle pigpioHandleRequest;
     getPigpioHandle.call(pigpioHandleRequest);
-    pigpio_handle = pigpioHandleRequest.response.handle;
+    pigpioHandle = pigpioHandleRequest.response.handle;
 
-    PigpioOutput pigpioOutput = PigpioOutput(&node, pigpio_handle);
+    PigpioOutput pigpioOutput(&node, pigpioHandle);
 
     ros::spin();
 
