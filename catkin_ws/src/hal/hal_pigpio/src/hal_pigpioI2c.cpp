6ca2e7e278b6e870395bac7c14ff522143567597
Arnaud HOTE <arnaud.hote@gmail.com>
2022-02-26 10:48:21 +0000

    Used initiliser list for constructor and made ROS debug messages prettier

--- catkin_ws/src/hal/hal_pigpio/src/hal_pigpioI2c.cpp
+++ catkin_ws/src/hal/hal_pigpio/src/hal_pigpioI2c.cpp
@@ -4,12 +4,12 @@
 #include "hal_pigpio/hal_pigpioGetHandle.h"
 
 PigpioI2c::PigpioI2c(ros::NodeHandle *node, int pigpioHandle) : pigpioHandle(pigpioHandle),
-                                                                getPigpioHandleClient(node->serviceClient<hal_pigpio::hal_pigpioGetHandle>("hal_pigpioGetHandle"))
+                                                                getPigpioHandleClient(node->serviceClient<hal_pigpio::hal_pigpioGetHandle>("hal_pigpioGetHandle")),
+                                                                i2cOpenService(node->advertiseService("hal_pigpioI2cOpen", &PigpioI2c::i2cOpen, this)),
+                                                                i2cCloseService(node->advertiseService("hal_pigpioI2cClose", &PigpioI2c::i2cClose, this)),
+                                                                i2cReadByteDataService(node->advertiseService("hal_pigpioI2cReadByteData", &PigpioI2c::i2cReadByteData, this)),
+                                                                i2cWriteByteDataService(node->advertiseService("hal_pigpioI2cWriteByteData", &PigpioI2c::i2cWriteByteData, this))
 {
-    i2cOpenService = node->advertiseService("hal_pigpioI2cOpen", &PigpioI2c::i2cOpen, this);
-    i2cCloseService = node->advertiseService("hal_pigpioI2cClose", &PigpioI2c::i2cClose, this);
-    i2cReadByteDataService = node->advertiseService("hal_pigpioI2cReadByteData", &PigpioI2c::i2cReadByteData, this);
-    i2cWriteByteDataService = node->advertiseService("hal_pigpioI2cWriteByteData", &PigpioI2c::i2cWriteByteData, this);
 }
 
 bool PigpioI2c::i2cOpen(hal_pigpio::hal_pigpioI2cOpen::Request &req,
@@ -19,7 +19,7 @@ bool PigpioI2c::i2cOpen(hal_pigpio::hal_pigpioI2cOpen::Request &req,
     if (res.handle >= 0)
     {
         res.hasSucceeded = true;
-        ROS_INFO("I2C bus %u open for device %u.", req.bus, req.address);
+        ROS_INFO("I2C bus %u open for device %u with handle %u.", req.bus, req.address, res.handle);
     }
     else
     {
@@ -52,12 +52,12 @@ bool PigpioI2c::i2cReadByteData(hal_pigpio::hal_pigpioI2cReadByteData::Request &
     if (res.value >= 0)
     {
         res.hasSucceeded = true;
-        ROS_INFO("Successfuly read register %u on I2C device %u.", req.deviceRegister, req.handle);
+        ROS_INFO("Successfuly read register %u on device with handle %u.", req.deviceRegister, req.handle);
     }
     else
     {
         res.hasSucceeded = false;
-        ROS_ERROR("Failed to read register %u on I2C device %u.", req.deviceRegister, req.handle);
+        ROS_ERROR("Failed to read register %u on device with handle %u.", req.deviceRegister, req.handle);
     }
     return true;
 }
@@ -65,15 +65,17 @@ bool PigpioI2c::i2cReadByteData(hal_pigpio::hal_pigpioI2cReadByteData::Request &
 bool PigpioI2c::i2cWriteByteData(hal_pigpio::hal_pigpioI2cWriteByteData::Request &req,
                                  hal_pigpio::hal_pigpioI2cWriteByteData::Response &res)
 {
-    if (i2c_write_byte_data(pigpioHandle, req.handle, req.deviceRegister, req.value) == 0)
+    int error = i2c_write_byte_data(pigpioHandle, req.handle, req.deviceRegister, req.value);
+    if (error == 0)
     {
         res.hasSucceeded = true;
-        ROS_INFO("Successfuly wrote register %u on I2C device %u.", req.deviceRegister, req.handle);
+        ROS_INFO("Successfuly wrote in register %u on device with handle %u.", req.deviceRegister, req.handle);
     }
     else
     {
         res.hasSucceeded = false;
-        ROS_ERROR("Failed to write register %u on I2C device %u.", req.deviceRegister, req.handle);
+        ROS_ERROR("Failed to write register %u on device with handle %u.", req.deviceRegister, req.handle);
+        ROS_ERROR("Error: %d.", error);
     }
     return true;
 }
