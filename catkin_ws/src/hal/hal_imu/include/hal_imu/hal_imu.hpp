77b98065de763dab99f03577e54f188810d3db66
Arnaud HOTE <arnaud.hote@gmail.com>
2022-03-02 16:30:02 +0000

    Moved functions from I2cInit and implemented reading from MPU function

--- catkin_ws/src/hal/hal_imu/include/hal_imu/hal_imu.hpp
+++ catkin_ws/src/hal/hal_imu/include/hal_imu/hal_imu.hpp
@@ -21,17 +21,25 @@ private:
     ImuClients *imuClients;
     ImuPublisher *imuPublisher;
     int32_t imuHandle;
+    int16_t angle;
 
 public:
     Imu(ImuPublisher *imuMessagePublisher, ImuClients *imuServiceClients);
     ~Imu() = default;
     void init(void);
     void writeDmp(void);
+    void enableDmp(void);
     void enable6AxisQuaternion(void);
     void calibrateAccelerometer(void);
     bool writeByteInRegister(uint8_t registerToWrite, uint8_t value);
+    int16_t readByteFromRegister(uint8_t registerToRead);
+    bool writeBitInRegister(uint8_t registerToWrite, uint8_t bitToWrite, uint8_t valueOfBit);
+    void setClockSource(void);
+    void setSleepDisabled(void);
     void enableGyroCalibrationOnDMP(void);
     bool writeDataToDmp(uint16_t address, uint8_t size, const unsigned char *data);
+    void publishMessage(void);
+    void readMpuData(void);
 };
 
 #endif
--- catkin_ws/src/hal/hal_imu/src/hal_imu.cpp
+++ catkin_ws/src/hal/hal_imu/src/hal_imu.cpp
@@ -45,10 +45,13 @@ Imu::Imu(ImuPublisher *imuMessagePublisher, ImuClients *imuServiceClients) : imu
 
 void Imu::init(void)
 {
-    this->writeDmp();
-    this->enable6AxisQuaternion();
-    this->calibrateAccelerometer();
-    this->enableGyroCalibrationOnDMP();
+    setClockSource();
+    setSleepDisabled();
+    writeDmp();
+    enable6AxisQuaternion();
+    enableDmp();
+    //    calibrateAccelerometer();
+    //    enableGyroCalibrationOnDMP();
 }
 
 void Imu::writeDmp(void)
@@ -74,6 +77,105 @@ void Imu::writeDmp(void)
     }
 }
 
+void Imu::enableDmp(void)
+{
+    /* Disable FIFO for accelerometer and gyroscope */
+    if (writeByteInRegister(MPU6050_FIFO_ENABLE_REGISTER, 0))
+    {
+        ROS_INFO("Sucessfully disabled FIFO for accelerometer and gyroscope.");
+    }
+    else
+    {
+        ROS_ERROR("Failed to disable FIFO for accelerometer and gyroscope!");
+    }
+
+    /* Enable interrupts for DMP */
+    if (writeByteInRegister(MPU6050_INTERRUPT_ENABLE_REGISTER, MPU6050_DMP_INTERRUPT_ONLY))
+    {
+        ROS_INFO("Sucessfully enabled interrupts for DMP.");
+    }
+    else
+    {
+        ROS_ERROR("Failed to enable interrupts for DMP!");
+    }
+}
+
+bool Imu::writeBitInRegister(uint8_t registerToWrite, uint8_t bitToWrite, uint8_t valueOfBit)
+{
+    hal_pigpio::hal_pigpioI2cReadByteData i2cReadByteDataSrv;
+    hal_pigpio::hal_pigpioI2cWriteByteData i2cWriteByteDataSrv;
+    uint8_t registerValue;
+    uint8_t newRegisterValue;
+
+    i2cReadByteDataSrv.request.handle = imuHandle;
+    i2cReadByteDataSrv.request.deviceRegister = registerToWrite;
+
+    imuClients->getReadByteDataClientHandle()->call(i2cReadByteDataSrv);
+
+    if (i2cReadByteDataSrv.response.hasSucceeded)
+    {
+        registerValue = i2cReadByteDataSrv.response.value;
+    }
+    else
+    {
+        return false;
+    }
+
+    if (valueOfBit == 1)
+    {
+        newRegisterValue = registerValue | (1 << bitToWrite);
+    }
+    else
+    {
+        newRegisterValue = registerValue & ~(1 << bitToWrite);
+    }
+
+    i2cWriteByteDataSrv.request.handle = imuHandle;
+    i2cWriteByteDataSrv.request.deviceRegister = registerToWrite;
+    i2cWriteByteDataSrv.request.value = newRegisterValue;
+
+    imuClients->getWriteByteDataClientHandle()->call(i2cWriteByteDataSrv);
+
+    if (i2cWriteByteDataSrv.response.hasSucceeded)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
+    return true;
+}
+
+void Imu::setClockSource(void)
+{
+    bool writeSuccess;
+
+    writeSuccess = writeBitInRegister(MPU6050_POWER_MANAGEMENT_1_REGISTER, MPU6050_CLOCK_SOURCE_BIT_1, 1) && writeBitInRegister(MPU6050_POWER_MANAGEMENT_1_REGISTER, MPU6050_CLOCK_SOURCE_BIT_2, 0) && writeBitInRegister(MPU6050_POWER_MANAGEMENT_1_REGISTER, MPU6050_CLOCK_SOURCE_BIT_3, 0);
+
+    if (writeSuccess)
+    {
+        ROS_INFO("Successfully enabled PLL_X clock source on device with handle %u.", imuHandle);
+    }
+    else
+    {
+        ROS_ERROR("Unable to enable PLL_X clock source on device with handle %u.", imuHandle);
+    }
+}
+
+void Imu::setSleepDisabled(void)
+{
+    if (writeBitInRegister(MPU6050_POWER_MANAGEMENT_1_REGISTER, MPU6050_ENABLE_SLEEP_BIT, 1))
+    {
+        ROS_INFO("Successfully disabled sleep mode on device with handle %u.", imuHandle);
+    }
+    else
+    {
+        ROS_ERROR("Unable to disable sleep mode on device with handle %u.", imuHandle);
+    }
+}
+
 void Imu::calibrateAccelerometer(void)
 {
 }
@@ -84,7 +186,7 @@ void Imu::enable6AxisQuaternion(void)
 
     if (writeDataToDmp(MPU6050_DMP_FEATURE_6X_LP_QUAT, MPU6050_DMP_FEATURE_6X_LP_QUAT_SIZE, dmp6AxisQuaternionEnable))
     {
-        ROS_INFO("Sucessfully enabled 6 axis quaternions on DMP!");
+        ROS_INFO("Sucessfully enabled 6 axis quaternions on DMP.");
     }
     else
     {
@@ -98,7 +200,7 @@ void Imu::enableGyroCalibrationOnDMP(void)
 
     if (writeDataToDmp(MPU6050_CFG_MOTION_BIAS, MPU6050_DMP_FEATURE_GYRO_CAL_SIZE, gyroscopeCalibrationEnable))
     {
-        ROS_INFO("Sucessfully enabled gyroscope calibration on DMP!");
+        ROS_INFO("Sucessfully enabled gyroscope calibration on DMP.");
     }
     else
     {
@@ -164,6 +266,90 @@ bool Imu::writeByteInRegister(uint8_t registerToWrite, uint8_t value)
     }
 }
 
+int16_t Imu::readByteFromRegister(uint8_t registerToRead)
+{
+    hal_pigpio::hal_pigpioI2cReadByteData i2cReadByteDataSrv;
+
+    i2cReadByteDataSrv.request.handle = imuHandle;
+    i2cReadByteDataSrv.request.deviceRegister = registerToRead;
+
+    imuClients->getReadByteDataClientHandle()->call(i2cReadByteDataSrv);
+
+    if (i2cReadByteDataSrv.response.hasSucceeded)
+    {
+        return i2cReadByteDataSrv.response.value;
+    }
+    else
+    {
+        ROS_ERROR("Failed to read byte!");
+        return -1;
+    }
+}
+
+void Imu::publishMessage(void)
+{
+    hal_imu::hal_imuMsg message;
+
+    message.angleInDeg = angle;
+    imuPublisher->publish(message);
+}
+
+void Imu::readMpuData(void)
+{
+    int16_t valueRead;
+    uint16_t fifoCount;
+    std::vector<uint8_t> fifoData;
+
+    /* Get number of bytes in the FIFO */
+    valueRead = readByteFromRegister(MPU6050_FIFO_COUNT_H_REGISTER);
+    if (valueRead >= 0)
+    {
+        fifoCount = ((uint8_t)valueRead) << 8;
+
+        valueRead = readByteFromRegister(MPU6050_FIFO_COUNT_L_REGISTER);
+        if (valueRead >= 0)
+        {
+            fifoCount |= ((uint8_t)valueRead);
+            ROS_INFO("Successfully read the number of bytes in the FIFO.");
+        }
+        else
+        {
+            ROS_ERROR("Failed to read the number of bytes in the FIFO!");
+        }
+    }
+    else
+    {
+        ROS_ERROR("Failed to read the number of bytes in the FIFO!");
+    }
+
+    if (fifoCount == MPU6050_DMP_FIFO_QUAT_SIZE)
+    {
+        for (int index = 0; index < MPU6050_DMP_FIFO_QUAT_SIZE; index++)
+        {
+            valueRead = readByteFromRegister(MPU6050_FIFO_REGISTER);
+
+            if (valueRead >= 0)
+            {
+                fifoData.push_back((uint8_t)valueRead);
+            }
+            else
+            {
+                ROS_ERROR("Failed to read FIFO!");
+                break;
+            }
+        }
+
+        if (fifoData.size() == MPU6050_DMP_FIFO_QUAT_SIZE)
+        {
+            ROS_ERROR("Sucessfully read FIFO!");
+        }
+    }
+    else
+    {
+        ROS_ERROR("Unexpected number of bytes in FIFO!");
+    }
+}
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "hal_imu");
@@ -179,6 +365,9 @@ int main(int argc, char **argv)
 
     while (ros::ok())
     {
+        imu.readMpuData();
+        imu.publishMessage();
+
         ros::spinOnce();
         loop_rate.sleep();
     }
