0ff90c5388bb06fbb785c5b0f6ff1f6379120275
Arnix <arnaud.hote@gmail.com>
2022-06-09 18:07:44 +0100

    Added task to clean and purge. Updated cross_compile.sh

--- cross_compile.sh
+++ cross_compile.sh
@@ -1,1 +1,1 @@
-ros_cross_compile ./catkin_ws/ --arch aarch64 --os ubuntu --rosdistro noetic --custom-data-dir ./library_pigpio/
+ros_cross_compile ./catkin_ws/ --arch aarch64 --os ubuntu --rosdistro humble --custom-data-dir ./library_pigpio/
