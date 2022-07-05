ca9b604abfdff260b1f21468334813f881b2e853
Arnix <arnaud.hote@gmail.com>
2022-07-05 16:16:37 +0100

    Updated hal_proxsens node to ROS2

---
+++ workspace/src/hal/hal_proxsens/include/hal_proxsens.hpp
@@ -0,0 +1,65 @@
+#ifndef HAL_PROXSENS
+#define HAL_PROXSENS
+
+#include "stdint.h"
+
+#include "rclcpp/rclcpp.hpp"
+
+// Services and messages headers (generated)
+#include "hal_pigpio/srv/hal_pigpio_set_input_mode.hpp"
+#include "hal_pigpio/srv/hal_pigpio_set_output_mode.hpp"
+#include "hal_pigpio/srv/hal_pigpio_set_gpio_high.hpp"
+#include "hal_pigpio/srv/hal_pigpio_send_trigger_pulse.hpp"
+#include "hal_pigpio/srv/hal_pigpio_set_callback.hpp"
+#include "hal_pigpio/msg/hal_pigpio_edge_change.hpp"
+#include "hal_pigpio/msg/hal_pigpio_heartbeat.hpp"
+#include "hal_proxsens/msg/hal_proxsens.hpp"
+
+#define PROXSENS_TRIGGER_GPIO 5
+#define PROXSENS_ECHO_GPIO 6
+#define PROXSENS_LEVEL_SHIFTER_OE_GPIO 10
+#define PROXSENS_TRIGGER_LENGTH_US 20
+
+#define AS_RISING_EDGE 0
+#define AS_FALLING_EDGE 1
+#define AS_EITHER_EDGE 2
+
+#define FALLING_EDGE 0
+#define RISING_EDGE 1
+#define NO_CHANGE 2
+
+class Proxsens
+{
+private:
+    std::shared_ptr<rclcpp::Node> halProxsensNode;
+    uint8_t edgeChangeType;
+    uint32_t timestamp;
+    uint32_t echoCallbackId;
+    uint16_t distanceInCm;
+    bool pigpioNodeStarted;
+    bool isStarted;
+    rclcpp::Client<hal_pigpio::srv::HalPigpioSetInputMode>::SharedPtr gpioSetInputClient;
+    rclcpp::Client<hal_pigpio::srv::HalPigpioSetOutputMode>::SharedPtr gpioSetOutputClient;
+    rclcpp::Client<hal_pigpio::srv::HalPigpioSetCallback>::SharedPtr gpioSetCallbackClient;
+    rclcpp::Client<hal_pigpio::srv::HalPigpioSendTriggerPulse>::SharedPtr gpioSendTriggerPulseClient;
+    rclcpp::Client<hal_pigpio::srv::HalPigpioSetGpioHigh>::SharedPtr gpioSetGpioHighClient;
+    rclcpp::Publisher<hal_proxsens::msg::HalProxsens>::SharedPtr proxsensDistancePub;
+    rclcpp::Subscription<hal_pigpio::msg::HalPigpioEdgeChange>::SharedPtr proxsensEdgeChangeSub;
+    rclcpp::Subscription<hal_pigpio::msg::HalPigpioHeartbeat>::SharedPtr proxsensPigpioHBSub;
+
+public:
+    Proxsens(std::shared_ptr<rclcpp::Node> node);
+    ~Proxsens() = default;
+    void publishDistance(void);
+    void configureGpios(void);
+    void trigger(void);
+    void enableOutputLevelShifter(void);
+    void edgeChangeCallback(const hal_pigpio::msg::HalPigpioEdgeChange &msg);
+    void pigpioHeartbeatCallback(const hal_pigpio::msg::HalPigpioHeartbeat &msg);
+    bool isPigpioNodeStarted(void);
+    void publishAndGetDistance(void);
+    bool isNotStarted(void);
+    void starts(void);
+};
+
+#endif
---
+++ workspace/src/hal/hal_proxsens/src/hal_proxsens.cpp
@@ -0,0 +1,179 @@
+#include "hal_proxsens.hpp"
+
+using namespace std::placeholders;
+using namespace std::chrono_literals;
+
+Proxsens::Proxsens(std::shared_ptr<rclcpp::Node> node) : halProxsensNode(node),
+                                                         edgeChangeType(NO_CHANGE),
+                                                         timestamp(0),
+                                                         echoCallbackId(0),
+                                                         distanceInCm(UINT16_MAX),
+                                                         pigpioNodeStarted(false),
+                                                         isStarted(false),
+                                                         gpioSetInputClient(node->create_client<hal_pigpio::srv::HalPigpioSetInputMode>("hal_pigpioSetInputMode")),
+                                                         gpioSetOutputClient(node->create_client<hal_pigpio::srv::HalPigpioSetOutputMode>("hal_pigpioSetOutputMode")),
+                                                         gpioSetCallbackClient(node->create_client<hal_pigpio::srv::HalPigpioSetCallback>("hal_pigpioSetCallback")),
+                                                         gpioSendTriggerPulseClient(node->create_client<hal_pigpio::srv::HalPigpioSendTriggerPulse>("hal_pigpioSendTriggerPulse")),
+                                                         gpioSetGpioHighClient(node->create_client<hal_pigpio::srv::HalPigpioSetGpioHigh>("hal_pigpioSetGpioHigh")),
+                                                         proxsensDistancePub(node->create_publisher<hal_proxsens::msg::HalProxsens>("proxSensorValue", 1000)),
+                                                         proxsensEdgeChangeSub(node->create_subscription<hal_pigpio::msg::HalPigpioEdgeChange>("gpioEdgeChange", 1000, std::bind(&Proxsens::edgeChangeCallback, this, _1))),
+                                                         proxsensPigpioHBSub(node->create_subscription<hal_pigpio::msg::HalPigpioHeartbeat>("hal_pigpioHeartbeat", 1000, std::bind(&Proxsens::pigpioHeartbeatCallback, this, _1)))
+{
+}
+
+void Proxsens::edgeChangeCallback(const hal_pigpio::msg::HalPigpioEdgeChange &msg)
+{
+    static uint8_t lastEdgeChangeType = NO_CHANGE;
+    static uint32_t lastTimestamp = 0;
+
+    uint32_t edgeLength = 0;
+
+    if (msg.gpio_id == PROXSENS_ECHO_GPIO)
+    {
+        edgeChangeType = msg.edge_change_type;
+        timestamp = msg.time_since_boot_us;
+
+        if ((edgeChangeType == FALLING_EDGE) && (lastEdgeChangeType == RISING_EDGE))
+        {
+            if (timestamp < lastTimestamp)
+            {
+                edgeLength = UINT32_MAX - lastTimestamp + timestamp;
+            }
+            else
+            {
+                edgeLength = timestamp - lastTimestamp;
+            }
+
+            distanceInCm = static_cast<uint16_t>(edgeLength / 58.0);
+        }
+        else if (edgeChangeType == RISING_EDGE)
+        {
+            lastEdgeChangeType = edgeChangeType;
+            lastTimestamp = timestamp;
+        }
+    }
+}
+
+void Proxsens::pigpioHeartbeatCallback(const hal_pigpio::msg::HalPigpioHeartbeat &msg)
+{
+    pigpioNodeStarted = msg.is_alive;
+}
+
+bool Proxsens::isPigpioNodeStarted(void)
+{
+    return pigpioNodeStarted;
+}
+
+void Proxsens::publishDistance(void)
+{
+    auto distance = hal_proxsens::msg::HalProxsens();
+
+    distance.distance_in_cm = distanceInCm;
+    proxsensDistancePub->publish(distance);
+}
+
+void Proxsens::configureGpios(void)
+{
+    auto setInputModeRequest = std::make_shared<hal_pigpio::srv::HalPigpioSetInputMode::Request>();
+    auto setOutputModeForTriggerRequest = std::make_shared<hal_pigpio::srv::HalPigpioSetOutputMode::Request>();
+    auto setOutputModeForShifterRequest = std::make_shared<hal_pigpio::srv::HalPigpioSetOutputMode::Request>();
+    auto setCallbackRequest = std::make_shared<hal_pigpio::srv::HalPigpioSetCallback::Request>();
+
+    setInputModeRequest->gpio_id = PROXSENS_ECHO_GPIO;
+    auto setInputModeResult = gpioSetInputClient->async_send_request(setInputModeRequest);
+    if (rclcpp::spin_until_future_complete(halProxsensNode, setInputModeResult) != rclcpp::FutureReturnCode::SUCCESS)
+    {
+        RCLCPP_ERROR(halProxsensNode->get_logger(), "Failed to call service setInputMode");
+    }
+
+    setCallbackRequest->gpio_id = PROXSENS_ECHO_GPIO;
+    setCallbackRequest->edge_change_type = AS_EITHER_EDGE;
+
+    auto setCallbackResult = gpioSetCallbackClient->async_send_request(setCallbackRequest);
+    if (rclcpp::spin_until_future_complete(halProxsensNode, setCallbackResult) == rclcpp::FutureReturnCode::SUCCESS)
+    {
+        if (setCallbackResult.get()->has_succeeded)
+        {
+            echoCallbackId = setCallbackResult.get()->callback_id;
+        }
+    }
+    else
+    {
+        RCLCPP_ERROR(halProxsensNode->get_logger(), "Failed to call service setCallback");
+    }
+
+    setOutputModeForTriggerRequest->gpio_id = PROXSENS_TRIGGER_GPIO;
+    auto setOutputModeForTriggerResult = gpioSetOutputClient->async_send_request(setOutputModeForTriggerRequest);
+    if (rclcpp::spin_until_future_complete(halProxsensNode, setOutputModeForTriggerResult) != rclcpp::FutureReturnCode::SUCCESS)
+    {
+        RCLCPP_ERROR(halProxsensNode->get_logger(), "Failed to call service setOutputMode for trigger");
+    }
+
+    setOutputModeForShifterRequest->gpio_id = PROXSENS_LEVEL_SHIFTER_OE_GPIO;
+    auto setOutputModeForShifterResult = gpioSetOutputClient->async_send_request(setOutputModeForShifterRequest);
+    if (rclcpp::spin_until_future_complete(halProxsensNode, setOutputModeForShifterResult) != rclcpp::FutureReturnCode::SUCCESS)
+    {
+        RCLCPP_ERROR(halProxsensNode->get_logger(), "Failed to call service setOutputMode for shifter");
+    }
+}
+
+void Proxsens::trigger(void)
+{
+    auto sendTriggerPulseRequest = std::make_shared<hal_pigpio::srv::HalPigpioSendTriggerPulse::Request>();
+
+    sendTriggerPulseRequest->gpio_id = PROXSENS_TRIGGER_GPIO;
+    sendTriggerPulseRequest->pulse_length_in_us = PROXSENS_TRIGGER_LENGTH_US;
+
+    auto sendTriggerPulseResult = gpioSendTriggerPulseClient->async_send_request(sendTriggerPulseRequest);
+}
+
+void Proxsens::enableOutputLevelShifter(void)
+{
+    auto setGpioHighRequest = std::make_shared<hal_pigpio::srv::HalPigpioSetGpioHigh::Request>();
+
+    setGpioHighRequest->gpio_id = PROXSENS_LEVEL_SHIFTER_OE_GPIO;
+    auto setGpioHighResult = gpioSetGpioHighClient->async_send_request(setGpioHighRequest);
+}
+
+void Proxsens::publishAndGetDistance(void)
+{
+    publishDistance();
+    trigger();
+}
+
+void Proxsens::starts(void)
+{
+    isStarted = true;
+}
+
+bool Proxsens::isNotStarted(void)
+{
+    return !isStarted;
+}
+
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<rclcpp::Node>("hal_proxsens_node");
+
+    Proxsens proxSens(node);
+
+    RCLCPP_INFO(node->get_logger(),"proxSens node waiting for pigpio node to start...");
+    while (rclcpp::ok())
+    {
+        if (proxSens.isNotStarted() && proxSens.isPigpioNodeStarted())
+        {
+            RCLCPP_INFO(node->get_logger(),"proxSens node initialising...");
+            proxSens.configureGpios();
+            proxSens.enableOutputLevelShifter();
+            proxSens.starts();
+            rclcpp::TimerBase::SharedPtr proxsensTimer(node->create_wall_timer(100ms, std::bind(&Proxsens::publishAndGetDistance, &proxSens)));
+            RCLCPP_INFO(node->get_logger(),"proxSens node initialised.");
+        }
+
+        rclcpp::spin_some(node);
+    }
+
+    rclcpp::shutdown();
+    return 0;
+}
