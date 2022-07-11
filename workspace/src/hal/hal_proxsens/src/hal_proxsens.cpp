0ba29d7e93264e36775abe4f55dc1d1cdc1d79fb
Arnix <arnaud.hote@gmail.com>
2022-07-11 18:20:14 +0100

    Removed ROS node code from existing cpp file

--- workspace/src/hal/hal_proxsens/src/hal_proxsens.cpp
+++ workspace/src/hal/hal_proxsens/src/hal_proxsens.cpp
@@ -150,30 +150,3 @@ bool Proxsens::isNotStarted(void)
 {
     return !isStarted;
 }
-
-int main(int argc, char **argv)
-{
-    rclcpp::init(argc, argv);
-    auto node = std::make_shared<rclcpp::Node>("hal_proxsens_node");
-
-    Proxsens proxSens(node);
-
-    RCLCPP_INFO(node->get_logger(),"proxSens node waiting for pigpio node to start...");
-    while (rclcpp::ok())
-    {
-        if (proxSens.isNotStarted() && proxSens.isPigpioNodeStarted())
-        {
-            RCLCPP_INFO(node->get_logger(),"proxSens node initialising...");
-            proxSens.configureGpios();
-            proxSens.enableOutputLevelShifter();
-            proxSens.starts();
-            rclcpp::TimerBase::SharedPtr proxsensTimer(node->create_wall_timer(100ms, std::bind(&Proxsens::publishAndGetDistance, &proxSens)));
-            RCLCPP_INFO(node->get_logger(),"proxSens node initialised.");
-        }
-
-        rclcpp::spin_some(node);
-    }
-
-    rclcpp::shutdown();
-    return 0;
-}
