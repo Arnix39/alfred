2b62c0d8d5126785da19f63c7db586d84596fd97
Arnix <arnaud.hote@gmail.com>
2022-09-19 10:38:42 +0200

    Implementation of mock Raspberry pi

--- workspace/src/hw/pigpio/src/mock/pigpio_mock.cpp
+++ workspace/src/hw/pigpio/src/mock/pigpio_mock.cpp
@@ -1,4 +1,22 @@
+// Copyright (c) 2022 Arnix Robotix
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include "pigpiod_if2.h"
+
+#include "mock/raspberrypi_mock.hpp"
+
+static RaspberryPi raspberryPi;
 
 /* I2C */
 int i2c_open(int pi, unsigned i2c_bus, unsigned i2c_addr, unsigned i2c_flags) {return 0;}
@@ -19,7 +37,17 @@ int i2c_write_i2c_block_data(int pi, unsigned handle, unsigned i2c_reg, char * b
 /* Init */
 int pigpio_start(const char * addrStr, const char * portStr) {return 0;}
 void pigpio_stop(int pi) {}
-int set_mode(int pi, unsigned gpio, unsigned mode) {return 0;}
+int set_mode(int pi, unsigned gpio, unsigned mode)
+{
+  if (mode == 0) {
+    raspberryPi.setGpioType(gpio, input);
+  }
+  else {
+    raspberryPi.setGpioType(gpio, output);
+  }
+
+  return 0;
+}
 int get_mode(int pi, unsigned gpio) {return 0;}
 int set_pull_up_down(int pi, unsigned gpio, unsigned pud) {return 0;}
 
