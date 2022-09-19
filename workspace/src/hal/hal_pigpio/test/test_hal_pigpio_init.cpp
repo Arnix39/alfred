ece8440a390a1982de991a235b52021468d3e404
Arnix <arnaud.hote@gmail.com>
2022-09-19 10:41:13 +0200

    First implemenation of unit test for hal_pigpio_init

--- workspace/src/hal/hal_pigpio/test/test_hal_pigpio_init.cpp
+++ workspace/src/hal/hal_pigpio/test/test_hal_pigpio_init.cpp
@@ -14,6 +14,88 @@
 
 #include "gtest/gtest.h"
 #include "rclcpp/rclcpp.hpp"
+#include "lifecycle_msgs/srv/change_state.hpp"
+
+#include "hal_pigpio.hpp"
+
+class PigioCheckerNode : public rclcpp::Node
+{
+public:
+  PigioCheckerNode()
+  : rclcpp::Node("hal_pigpio_checker_node"),
+    changeStateClient(this->create_client<lifecycle_msgs::srv::ChangeState>(
+        "hal_pigpio_node/change_state")),
+    setInputModeClient(this->create_client<hal_pigpio_interfaces::srv::HalPigpioSetInputMode>(
+        "hal_pigpioSetInputMode"))
+  {
+  }
+  ~PigioCheckerNode() = default;
+  void changePigpioNodeToState(std::uint8_t transition)
+  {
+    auto request = std::make_shared<lifecycle_msgs::srv::ChangeState::Request>();
+    request->transition.id = transition;
+    auto result = changeStateClient->async_send_request(request);
+  }
+  rclcpp::Client<hal_pigpio_interfaces::srv::HalPigpioSetInputMode>::SharedPtr
+  getSetInputModeClient(void)
+  {
+    return setInputModeClient;
+  }
+
+private:
+  rclcpp::Client<lifecycle_msgs::srv::ChangeState>::SharedPtr changeStateClient;
+  rclcpp::Client<hal_pigpio_interfaces::srv::HalPigpioSetInputMode>::SharedPtr setInputModeClient;
+};
+
+/* Test fixture */
+class PigpioInitTest : public testing::Test
+{
+protected:
+  std::shared_ptr<Pigpio> pigpioInit;
+  std::shared_ptr<PigioCheckerNode> pigioChecker;
+  rclcpp::executors::SingleThreadedExecutor executor;
+
+  void SetUp()
+  {
+    pigioChecker = std::make_shared<PigioCheckerNode>();
+    pigpioInit = std::make_shared<Pigpio>();
+
+    executor.add_node(pigpioInit->get_node_base_interface());
+    executor.add_node(pigioChecker);
+
+    pigioChecker->changePigpioNodeToState(
+      lifecycle_msgs::msg::Transition::TRANSITION_CONFIGURE);
+    executor.spin_some();
+    /*pigioChecker->changePigpioNodeToState(
+      lifecycle_msgs::msg::Transition::TRANSITION_ACTIVATE);
+    executor.spin_some();*/
+  }
+
+  void TearDown()
+  {
+    executor.cancel();
+    executor.remove_node(pigpioInit->get_node_base_interface());
+    executor.remove_node(pigioChecker);
+    pigpioInit.reset();
+    pigioChecker.reset();
+  }
+};
+
+/* Test cases */
+TEST_F(PigpioInitTest, SetInputMode)
+{
+  auto setInputModeRequest =
+    std::make_shared<hal_pigpio_interfaces::srv::HalPigpioSetInputMode::Request>();
+
+  setInputModeRequest->gpio_id = 1;
+
+  auto setInputModeFuture = pigioChecker->getSetInputModeClient()->async_send_request(
+    setInputModeRequest);
+
+  //executor.spin_some();
+
+  ASSERT_EQ(setInputModeFuture.get()->has_succeeded, true);
+}
 
 int main(int argc, char ** argv)
 {
