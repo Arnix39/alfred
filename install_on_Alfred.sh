73c9db8bbf7d91f6f3856eeb88cc789bf0ee0699
Arnix <arnaud.hote@gmail.com>
2022-10-25 14:44:18 +0200

    Removed source command in install script

--- install_on_Alfred.sh
+++ install_on_Alfred.sh
@@ -2,6 +2,4 @@ echo "Removing old files..."
 ssh arnix@alfred_ros2 'rm -r /home/arnix/Alfred/'
 echo "Copying new files..."
 scp -r ./workspace/install/ arnix@alfred_ros2:/home/arnix/Alfred/
-echo "Sourcing environment..."
-ssh arnix@alfred_ros2 'source /home/arnix/Alfred/setup.bash'
 echo "Installation done!"
