6cacbf76e2c46bf2d9c9c77ed1f5919d3297d4d8
Arnix <arnaud.hote@gmail.com>
2022-11-28 18:25:22 +0100

    Created task to install application on Alfred

--- scripts/install_on_Alfred.sh
+++
@@ -1,5 +0,0 @@
-echo "Removing old files..."
-ssh arnix@alfred_ros2 'rm -r /home/arnix/Alfred/'
-echo "Copying new files..."
-scp -r $1/ arnix@alfred_ros2:/home/arnix/Alfred/
-echo "Installation done!"
