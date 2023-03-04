27b46bac35f48e1f24f3dd2861158f8919732586
Arnix <arnaud.hote@gmail.com>
2023-03-04 20:08:37 +0100

    Added ros_gz as a git submodule + updated CI scripts and VSCode tasks

--- scripts/Jenkins_host_build.sh
+++ scripts/Jenkins_host_build.sh
@@ -2,6 +2,6 @@
 
 . /opt/ros/humble/setup.sh
 cd workspace
-colcon build --build-base build/host --install-base install/host --symlink-install --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DSYNTHETIC_BUILD:BOOL=ON --no-warn-unused-cli
-colcon test --build-base build/host --install-base install/host
+colcon build --build-base build/host --install-base install/host --packages-skip-regex ros_gz --symlink-install --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DSYNTHETIC_BUILD:BOOL=ON --no-warn-unused-cli
+colcon test --build-base build/host --install-base install/host --packages-skip-regex ros_gz
 colcon test-result --test-result-base build/host
