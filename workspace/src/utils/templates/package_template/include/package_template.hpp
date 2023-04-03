34c9f71cc98f60469b5d6d076d5bfebaf5e8e8db
Arnix <arnaud.hote@gmail.com>
2023-04-03 17:03:30 +0200

    Updated package template

--- workspace/src/utils/templates/package_template/include/package_template.hpp
+++ workspace/src/utils/templates/package_template/include/package_template.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2022 Arnix Robotix
+// Copyright (c) 2023 Arnix Robotix
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -31,4 +31,4 @@ public:
   LifecycleCallbackReturn_t on_error(const rclcpp_lifecycle::State & previous_state);
 };
 
-#endif  // PACKAGE_TEMPLATE_HPP_"
+#endif  // PACKAGE_TEMPLATE_HPP_
