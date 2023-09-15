f83b3394587a3ba76cea6fd034256e2d18067938
Arnix <arnaud.hote@gmail.com>
2023-09-15 16:31:23 +0200

    Removed Jenkins CI scripts

--- scripts/Jenkins_host_build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-. /opt/ros/humble/setup.sh
-cd workspace
-colcon build --build-base build/host --install-base install/host --packages-skip-regex ros_gz --symlink-install --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DSYNTHETIC_BUILD:BOOL=ON --no-warn-unused-cli
-colcon test --build-base build/host --install-base install/host --packages-skip-regex ros_gz
-colcon test-result --test-result-base build/host
