c793b2b4aa8fe56bb930568f295917aebaf491e9
Arnix <arnaud@olearo-hote.eu>
2024-10-04 11:54:37 +0200

    fix: used docker buildx to generate sysroot

--- alfred/cross_compile/generate_sysroot.sh
+++ alfred/cross_compile/generate_sysroot.sh
@@ -8,7 +8,8 @@ cp -r ../src workspace
 rm -r workspace/src/sim
 
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-docker build -t arm_ros2:humble -f ./Dockerfile/Dockerfile_arm64_humble .
+docker buildx create --use --name multi-arch-builder
+docker buildx build --platform linux/arm64 -t arm_ros2:humble -f ./Dockerfile/Dockerfile_arm64_humble .
 docker run --name alfred_cross arm_ros2:humble
 
 docker container export -o sysroot.tar alfred_cross
