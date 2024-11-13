d4b31004836e5ca1e95d94a1c52adec80387ee30
Arnix <arnaud@olearo-hote.eu>
2024-11-13 12:49:31 +0100

    fix: fixed linter errors in app launch file

--- alfred/src/app/app_lifecycle_manager/launch/alfred_app_launch.py
+++ alfred/src/app/app_lifecycle_manager/launch/alfred_app_launch.py
@@ -1,3 +1,17 @@
+# Copyright (c) 2024 Arnix Robotix
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import launch
 import launch.actions
 import launch.events
@@ -8,6 +22,7 @@ import launch_ros.events.lifecycle
 
 import lifecycle_msgs.msg
 
+
 def generate_launch_description():
     ld = launch.LaunchDescription()
 
@@ -15,15 +30,16 @@ def generate_launch_description():
         name='app_controller', namespace='',
         package='app_controller', executable='app_controller_node', output='screen')
 
-    register_event_handler_for_app_controller_reaches_active_state = launch.actions.RegisterEventHandler(
-        launch_ros.event_handlers.OnStateTransition(
-            target_lifecycle_node=app_controller_node, goal_state='active',
-            entities=[
-                launch.actions.LogInfo(
-                    msg="app_controller_node active"),
-            ],
+    register_event_handler_for_app_controller_reaches_active_state = \
+        launch.actions.RegisterEventHandler(
+            launch_ros.event_handlers.OnStateTransition(
+                target_lifecycle_node=app_controller_node, goal_state='active',
+                entities=[
+                    launch.actions.LogInfo(
+                        msg="app_controller_node active"),
+                ],
+            )
         )
-    )
 
     emit_event_to_request_app_controller_configure_transition = launch.actions.EmitEvent(
         event=launch_ros.events.lifecycle.ChangeState(
