31895c4b84909c3f291c9599faca9bb0cecf8c8d
Arnix <arnaud@olearo-hote.eu>
2024-12-16 13:10:24 +0100

    fix: Updated dev container config

--- alfred/.devcontainer/postCreate.bash
+++ alfred/.devcontainer/postCreate.bash
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-sudo rosdep update
-sudo apt update
-sudo rosdep install --from-paths src --ignore-src -r -i -y --rosdistro humble
+rosdep update
+rosdep install --from-paths src --ignore-src -r -i -y --rosdistro humble
 
 echo ". /opt/ros/humble/setup.bash" | sudo tee -a ~/.bashrc > /dev/null
